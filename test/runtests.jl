a5839481077f08aac3b367dc8a9bdb31d8dcf722
lassepe <lasse.peters@mailbox.org>
2024-02-15 01:13:05 +0100

    Add integration tests for overtaking example

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,19 @@ using Test: @test, @testset
 
 @testset "ContingencyGames.jl" begin
     @testset "integration tests" begin
-        # minimal integration test to exercise the interactive demo
         @testset "crosswalk nonlinear demo" begin
             demo_setup = ContingencyGames.Examples.setup_crosswalk_nonlinear_demo()
+            info = ContingencyGames.Examples.demo(
+                demo_setup;
+                sim_kwargs = (; check_termination = function (state, step)
+                    step > 1
+                end),
+            )
+            @test info.feasible
+        end
+
+        @testset "overtaking nonlinear demo" begin
+            demo_setup = ContingencyGames.Examples.setup_overtaking_nonlinear_demo()
             info = ContingencyGames.Examples.demo(
                 demo_setup;
                 sim_kwargs = (; check_termination = function (state, step)
